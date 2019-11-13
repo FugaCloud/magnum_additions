2307d94d95a2ae6cb06b8fe2f899d2c5e2a117eb
Tony Scheffer <tony@fuga.cloud>
2019-11-13 09:27:43 +0100

    removed: creation of subnet and network since magnum does not apply sg's

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -25,15 +25,16 @@ dhcp_end="10.65.0.20"
 # when using a name is breaks because it cannot find os_distro?
 image="ac6c15cc-9073-4537-98d9-00f4ccfefa25"
 
-# create network and disable port security because of Contrail issue
-openstack network create --no-share --disable-port-security ${name}-network
+if ! openstack coe cluster template create --image "$image" --external-network public --master-flavor $master_flavor --flavor $node_flavor --coe kubernetes --volume-driver cinder --network-driver flannel --docker-volume-size 40 ${name}-template
+  then
+    echo "error while creating the cluster template"
+    exit 1
+fi
 
-# create subnet
-openstack subnet create --dhcp --network ${name}-network --subnet-range $cidr --allocation-pool start=${dhcp_start},end=${dhcp_end} ${name}-subnet
-
-# we always first need a template, we need to define our networks, if magnum makes one for us, it will apply security groups
-openstack coe cluster template create --image "$image" --external-network public --fixed-network ${name}-network --fixed-subnet ${name}-subnet --dns-nameserver 8.8.8.8 --master-flavor $master_flavor --flavor $node_flavor --coe kubernetes --volume-driver cinder --network-driver flannel --docker-volume-size 40 ${name}-template
-
-# finally, create the cluster
-openstack coe cluster create --cluster-template ${name}-template --master-count $master_count --node-count $node_count --keypair $keypair ${name}-cluster
+# create the cluster
+if ! openstack coe cluster create --cluster-template ${name}-template --master-count $master_count --node-count $node_count --keypair $keypair ${name}-cluster
+  then
+    echo "error while creating cluster"
+    exit 1
+fi
 
